87199e0a25c0f43c2be4a23d769b5ad5d4425118
Saqif Badruddin <saqif280@gmail.com>
2021-10-01 01:09:38 -0400

    Add social icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.scss';
+import { IconContext } from "react-icons";
+import { FaDiscord, FaTwitter } from 'react-icons/fa';
 
 function App() {
   return (
@@ -7,6 +9,10 @@ function App() {
       <div className="section--hero">
         <h1>Lineage</h1>
         <p>Shape the world - it’s yours after all.</p>
+        <div className="socials">
+          <a className="link--social" href="#"><FaDiscord /></a>
+          <a className="link--social" href="#"><FaTwitter /></a>
+        </div>
       </div>
     </div>
   );
