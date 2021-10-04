a0b56250a5e2fafc2a39682df32d88ccb3d49a8f
Saqif Badruddin <saqif280@gmail.com>
2021-10-04 01:01:58 -0400

    Add more socials

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import logo from './logo.svg';
 import './App.scss';
 import { IconContext } from "react-icons";
-import { FaDiscord, FaTwitter } from 'react-icons/fa';
+import { FaDiscord, FaTwitter, FaInstagram, FaMedium } from 'react-icons/fa';
 
 function App() {
   return (
     <div className="app">
       <div className="section--hero">
         <h1>Lineage</h1>
-        <p>Shape the world - it’s yours after all.</p>
+        {/* <p>Shape the world - it’s yours after all.</p> */}
         <div className="socials">
-          <a className="link--social" href="#"><FaDiscord /></a>
-          <a className="link--social" href="#"><FaTwitter /></a>
+          <a className="link--social" target="_blank" href="https://discord.gg/EVhzumCtMd"><FaDiscord /></a>
+          <a className="link--social" target="_blank" href="https://twitter.com/lineagenft"><FaTwitter /></a>
+          <a className="link--social" target="_blank" href="https://www.instagram.com/lineagenft/"><FaInstagram /></a>
+          <a className="link--social" target="_blank" href="https://medium.com/@lineagenft"><FaMedium /></a>
         </div>
       </div>
     </div>
