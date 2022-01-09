dff9af59125298f21b7981200d16850d49087a08
Lucas Della Bella <lucas@safegraph.com>
2022-01-09 11:49:51 -0800

    Install ethers, add some sample code from other repo into lineage-viewer page

--- src/LineageViewer.js
+++ src/LineageViewer.js
@@ -10,6 +10,50 @@ const containerStyles = {
   height: "100vh",
 };
 
+// require("dotenv").config({ path: "../../lineage/.env" });
+// const cliArgs = process.argv.slice(2);
+
+// const ethers = require("ethers");
+
+// const API_URL = process.env.API_URL;
+// let PUBLIC_KEY;
+// let PRIVATE_KEY;
+
+// if (API_URL.includes("localhost")) {
+//   PUBLIC_KEY = process.env.LOCALHOST_PUBLIC_KEY;
+//   PRIVATE_KEY = process.env.LOCALHOST_PRIVATE_KEY;
+// } else {
+//   PUBLIC_KEY = process.env.PUBLIC_KEY;
+//   PRIVATE_KEY = process.env.PRIVATE_KEY;
+// }
+
+// const LATEST_CONTRACT_ADDRESS = process.env.LATEST_CONTRACT_ADDRESS;
+// // test
+// const contract = require("../../lineage/artifacts/contracts/MyNFT.sol/MyNFT.json");
+
+// // Ethers setup
+// const provider = new ethers.providers.JsonRpcProvider();
+// const signer = provider.getSigner();
+// const nftContract = new ethers.Contract(
+//   LATEST_CONTRACT_ADDRESS,
+//   contract.abi,
+//   provider
+// );
+
+// // Set the default signer for all functions that write to the network
+// const nftContractWithSigner = nftContract.connect(signer);
+
+const useCenteredTree = () => {
+  const [translate, setTranslate] = useState({ x: 0, y: 0 });
+  const containerRef = useCallback((containerElem) => {
+    if (containerElem !== null) {
+      const { width, height } = containerElem.getBoundingClientRect();
+      setTranslate({ x: width / 2, y: height / 5 });
+    }
+  }, []);
+  return [translate, containerRef];
+};
+
 // Here we're using `renderCustomNodeElement` render a component that uses
 // both SVG and HTML tags side-by-side.
 // This is made possible by `foreignObject`, which wraps the HTML tags to
@@ -34,18 +78,6 @@ const renderForeignObjectNode = ({
     </foreignObject>
   </g>
 );
-
-const useCenteredTree = () => {
-  const [translate, setTranslate] = useState({ x: 0, y: 0 });
-  const containerRef = useCallback((containerElem) => {
-    if (containerElem !== null) {
-      const { width, height } = containerElem.getBoundingClientRect();
-      setTranslate({ x: width / 2, y: height / 2 });
-    }
-  }, []);
-  return [translate, containerRef];
-};
-
 const LineageViewer = () => {
   useNavbarScrollPrevention();
   const [translate, containerRef] = useCenteredTree();
@@ -60,6 +92,9 @@ const LineageViewer = () => {
             data={sampleData}
             translate={translate}
             nodeSize={nodeSize}
+            renderCustomNodeElement={(rd3tProps) =>
+              renderForeignObjectNode({ ...rd3tProps, foreignObjectProps })
+            }
             orientation="vertical"
           />
         </div>
