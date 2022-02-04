738d6f7a37303137fdf333f7c5661dcf0f2defad
Saqif Badruddin <saqif280@gmail.com>
2022-02-03 23:33:43 -0800

    fix periods

--- src/Roadmap.js
+++ src/Roadmap.js
@@ -18,8 +18,8 @@ const Roadmap = () => {
               <p>We have a strong foundational belief that roadmaps <b>should not</b> be set in stone before the community is grown. That being said, CRESTs are designed like Loot, to expose their data for those that want to create on top. We're builders at heart and so we're super excited to build more for the community.</p>
               <p>Some ideas we're exploring;
                 <ul>
-                  <li>Launch an 8000 piece <b>PFP project</b>, 50% of the collection reserved for OG crest holders.</li>
-                  <li>Have stories and <b>lore professionally written and narrated</b> for the community, and run auctions where 100% of funds go to charity and back to the community.</li>
+                  <li>Launch an 8000 piece <b>PFP project</b>, 50% of the collection reserved for OG crest holders</li>
+                  <li>Have stories and <b>lore professionally written and narrated</b> for the community, and run auctions where 100% of funds go to charity and back to the community</li>
                   <li>Create a <b>Lineage D&D campaign guide</b> set in the world of Kharth</li>
                   <li>Design a <b>Lineage DAO</b> that allows the community to contribute and canonize lore on chain to grow the intellectual property</li>
                   <li>Spin up a <b>Lineage Merch Store</b>, <b>Twitter Banner Creator</b>, and other fun things for the community</li>
