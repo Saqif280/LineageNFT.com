44556b442c7f68969e92beeaa1d92f34b58ec445
Saqif Badruddin <saqif280@gmail.com>
2022-02-27 17:02:24 -0800

    Add mint functionality to mint button

--- src/Mint.js
+++ src/Mint.js
@@ -1,12 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import classNames from 'classnames';
 import Crest from './img/crests/crest_backplate.png';
+import { ethers } from "ethers";
+import CONTRACT from './contract.json';
 
 const Mint = () => {
   const [mintedCount, setMintedCount] = useState(0);
-  const handleClick = () => {
-    alert('this feature isn\'t ready yet!');
+  const [statusText, setStatusText] = useState();
+  const [waitingForMintSignature, setWaitingForMintSignature] = useState(false);
+  const CONTRACT_ADDRESS = '0xBBedB508F864f828155389BA8a9d13ACFE65C29f';
+
+  // let account;
+  let contract;
+
+  useEffect(async () => {
+    // A Web3Provider wraps a standard Web3 provider, which is
+    // what MetaMask injects as window.ethereum into each page
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    // MetaMask requires requesting permission to connect users accounts
+    await provider.send("eth_requestAccounts", []);
+
+    // The MetaMask plugin also allows signing transactions to
+    // send ether and pay to change state within the blockchain.
+    // For this, you need the account signer...
+    const signer = provider.getSigner();
+    contract = new ethers.Contract(
+      CONTRACT_ADDRESS,
+      CONTRACT.abi,
+      signer
+    );
+
+    // get the contract minted count to populate the UI
+    updateMintCount();
+  });
+
+  const updateMintCount = async () => {
+    try {
+      setMintedCount((await contract.totalSupply()).toNumber());
+    } catch (error) {
+      console.error('Having some issue calling the contract - ',error);
+    }
   }
+
+  const handleFulfilledMint = () => {
+    setStatusText('Mint transaction successfully signed!');
+    setWaitingForMintSignature(false);
+  }
+
+  const handleRejectedMint = () => {
+    setStatusText('Mint transaction signing failed');
+    setWaitingForMintSignature(false);
+  }
+
+  const handleClick = () => {
+    try {
+      setWaitingForMintSignature(true);
+      const mintPromise = contract.mintNFT(1)
+        .then(handleFulfilledMint, handleRejectedMint);
+    } catch (error) {
+      console.error('Having some issue calling the contract - ',error);
+    }
+  }
+
   return (
     <div className="section section--mint" id="mint">
       <div className="content">
@@ -17,7 +73,15 @@ const Mint = () => {
               <h2>Mint a Crest</h2>
               <p>Claim your Kharthian family by minting a crest below.</p>
               <p><b>{mintedCount.toString()}/4000 minted</b></p>
-              <button className="button" onClick={handleClick}>mint</button>
+              {waitingForMintSignature ?
+                (
+                  <button className="button" onClick={handleClick} disabled>Waiting for signature ...</button>
+                ) :
+                (
+                  <button className="button" onClick={handleClick}>Mint</button>
+                )
+              }
+              <p><i>{statusText}</i></p>
             </div>
           </div>
         </div>
